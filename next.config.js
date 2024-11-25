1508da208e565914f7c15dfb9e4589f1b7d09ef4
Qiu Qiu <supersuperbruce@gmail.com>
2024-11-25 21:24:51 +0800

    11252124

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = 'freeai.world';
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '',
-  assetPrefix: './',
-  trailingSlash: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 }
 
 module.exports = nextConfig;
