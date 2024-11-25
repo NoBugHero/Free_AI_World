3c1c6b9c41c6e849578b6858171b87d8ba7f8471
Qiu Qiu <supersuperbruce@gmail.com>
2024-11-25 21:43:23 +0800

    Update GitHub Actions workflow with debug info

--- next.config.js
+++ next.config.js
@@ -1,22 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  const repo = 'freeai.world';
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  basePath: process.env.GITHUB_ACTIONS ? '/freeai.world' : '',
+  assetPrefix: process.env.GITHUB_ACTIONS ? '/freeai.world/' : '',
+  trailingSlash: true,
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
