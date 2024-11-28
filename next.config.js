18a70326887dba37f03a04114b88196badecbe0f
Qiu Qiu <supersuperbruce@gmail.com>
2024-11-28 12:10:33 +0800

    11281210

--- next.config.js
+++ next.config.js
@@ -6,13 +6,11 @@ const nextConfig = {
   // 生产环境配置
   ...(process.env.NODE_ENV === 'production' ? {
     basePath: '',
-    // 使用相对路径或完整的域名
-    assetPrefix: 'https://freeai.world',
-    // 或者使用相对路径
-    // assetPrefix: '',
+    assetPrefix: '.',
+    trailingSlash: true,
   } : {}),
   // 添加输出配置
-  output: 'export',  // 用于静态网站导出
+  output: 'export',
 }
 
 module.exports = nextConfig
