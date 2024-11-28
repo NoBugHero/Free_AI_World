ffa977df76f656ad9b31ca62b39f40d2c5a56511
Qiu Qiu <supersuperbruce@gmail.com>
2024-11-28 15:13:48 +0800

    Setup GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,15 +2,16 @@ const { env } = require('./config/environment')
 
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'Free_AI_World' // 你的仓库名称
 
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/Free_AI_World' : '',
-  assetPrefix: isProd ? '/Free_AI_World' : '',
-  trailingSlash: true,
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
