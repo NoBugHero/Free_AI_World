4da6248f4a502939c047c8ea929ee368a80f5b77
Qiu Qiu <supersuperbruce@gmail.com>
2024-11-28 13:57:45 +0800

    11281357

--- next.config.js
+++ next.config.js
@@ -1,20 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 开发环境配置
-  basePath: '',
-  assetPrefix: '',
-  // 生产环境配置
-  ...(process.env.NODE_ENV === 'production' ? {
-    basePath: '',
-    assetPrefix: './',
-    trailingSlash: true,
-  } : {}),
-  // 添加输出配置
   output: 'export',
-  // 样式配置
+  assetPrefix: './',
+  trailingSlash: true,
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
+  basePath: '',
 }
 
 module.exports = nextConfig
