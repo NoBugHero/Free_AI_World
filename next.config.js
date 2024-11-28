bf3c02951b42fd4d7fe293b2e519730efae6978c
Qiu Qiu <supersuperbruce@gmail.com>
2024-11-28 14:50:52 +0800

    11281450

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 const { env } = require('./config/environment')
 
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   output: 'export',
-  basePath: '',
-  assetPrefix: './',
+  basePath: isProd ? '/Free_AI_World' : '',
+  assetPrefix: isProd ? '/Free_AI_World' : '',
   trailingSlash: true,
   images: {
     unoptimized: true,
